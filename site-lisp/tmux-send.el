2eb572da3ea9d73386f2a5b2ce09f32d19935df0
Ian Yang <me@iany.me>
2013-05-03 03:05:46 +0800

    Add confirm before sending region to tmux

--- site-lisp/tmux-send.el
+++ site-lisp/tmux-send.el
@@ -33,21 +33,23 @@
   "Send selected region or currently-surrounding blank line-separated \
 block of text to the selected tmux session."
   (interactive "P")
-  (when (not tmux-session)
-    (call-interactively 'tmux-select))
-  (let ((selected (if (region-active-p)
-                      (buffer-substring-no-properties (region-beginning) (region-end))
-                    (read-from-minibuffer "Command: " (car tmux-send-hist) nil nil '(tmux-send-hist . 1))))
-        (tmpfile (make-temp-file "tmux-send.")))
-    (when (region-active-p)
-      (add-to-history 'tmux-send-hist selected))
-    (with-temp-file tmpfile
-      (insert selected)
-      (unless no-new-line (insert "\n")))
-    (call-process "tmux" nil nil nil
-                  "load-buffer" tmpfile ";"
-                  "paste-buffer" "-t" tmux-session ";")
-    (delete-file tmpfile)
-    (deactivate-mark)))
+  ;; prompt at least once
+  (unless (and tmux-session (region-active-p) (not (y-or-n-p (format "Send region to %s?" tmux-session))))
+    (when (not tmux-session)
+      (call-interactively 'tmux-select))
+    (let ((selected (if (region-active-p)
+                        (buffer-substring-no-properties (region-beginning) (region-end))
+                      (read-from-minibuffer "Command: " nil nil nil 'tmux-send-hist)))
+          (tmpfile (make-temp-file "tmux-send.")))
+      (when (region-active-p)
+        (add-to-history 'tmux-send-hist selected))
+      (with-temp-file tmpfile
+        (insert selected)
+        (unless no-new-line (insert "\n")))
+      (call-process "tmux" nil nil nil
+                    "load-buffer" tmpfile ";"
+                    "paste-buffer" "-t" tmux-session ";")
+      (delete-file tmpfile)
+      (deactivate-mark))))
 
 (provide 'tmux-session)
